da8643cb11766f1cc3064dfb4e6c069e0a21cbec
ARTEM <girukartem@gmail.com>
2021-04-12 17:55:55 +0300

    express orders

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
 
+  if($('.js_cooperation__slider').length > 0 && $(window).width() > 1100) {
+    $('.js_cooperation__slider').slick({
+      infinite: true,
+      arrows: false,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      speed: 500,
+      dots: true,
+      responsive: [
+        {
+          breakpoint:800,
+          settings: {
+            slidesToShow: 1.05,
+            slidesToScroll: 1,
+          }
+        }
+
+      ]
+    });
+  }
+
   if($('.js_slider-reviews').length > 0) {
     $('.js_slider-reviews').slick({
       infinite: true,
