cc4a7b00dc8e644618d40ef7bb727abe239aba6e
ihor139 <igor.radchuk.13@gmail.com>
2021-04-13 14:57:29 +0300

    Merge branch 'master' of https://github.com/HiruArt/sklad-usa-website
    # Conflicts:
    #	src/js/slider.js
    #	src/pug/_layout.pug

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,107 +1,131 @@
 $(document).ready(function () {
 
-    if ($('.js_cooperation__slider').length > 0 && $(window).width() > 1100) {
-        $('.js_cooperation__slider').slick({
-            infinite: true,
-            arrows: false,
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            speed: 500,
-            dots: true,
-            responsive: [
-                {
-                    breakpoint: 800,
-                    settings: {
-                        slidesToShow: 1.05,
-                        slidesToScroll: 1,
-                    }
-                }
+	if ($('.js_cooperation__slider').length > 0 && $(window).width() > 1100) {
+		$('.js_cooperation__slider').slick({
+			infinite: true,
+			arrows: false,
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			speed: 500,
+			dots: true,
+			responsive: [
+				{
+					breakpoint: 800,
+					settings: {
+						slidesToShow: 1.05,
+						slidesToScroll: 1,
+					}
+				}
 
-            ]
-        });
-    }
+			]
+		});
+	}
 
-    if ($('.js_slider-reviews').length > 0) {
-        $('.js_slider-reviews').slick({
-            infinite: true,
-            arrows: false,
-            slidesToShow: 4,
-            slidesToScroll: 2,
-            speed: 500,
-            dots: true,
-            responsive: [
-                {
-                    breakpoint: 1500,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 2,
-                    }
-                },
-                {
-                    breakpoint: 1100,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
-                    }
-                },
-                {
-                    breakpoint: 550,
-                    settings: {
-                        slidesToShow: 1.05,
-                        slidesToScroll: 1,
-                    }
-                }
+	if ($('.js_slider-reviews').length > 0) {
+		$('.js_slider-reviews').slick({
+			infinite: true,
+			arrows: false,
+			slidesToShow: 4,
+			slidesToScroll: 2,
+			speed: 500,
+			dots: true,
+			responsive: [
+				{
+					breakpoint: 1500,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 2,
+					}
+				},
+				{
+					breakpoint: 1100,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2
+					}
+				},
+				{
+					breakpoint: 550,
+					settings: {
+						slidesToShow: 1.05,
+						slidesToScroll: 1,
+					}
+				}
 
-            ]
-        });
-    }
+			]
+		});
+	}
 
-    if ($('.other-article__items').length > 0) {
-        $('.other-article__items').slick({
-            infinite: true,
-            arrows: false,
-            slidesToShow: 4,
-            slidesToScroll: 2,
-            speed: 500,
-            dots: true,
-            responsive: [
-                {
-                    breakpoint: 1300,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 2,
-                    }
-                },
-                {
-                    breakpoint: 1100,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
-                    }
-                },
-                {
-                    breakpoint: 550,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                    }
-                }
+	if ($('.other-article__items').length > 0) {
+		$('.other-article__items').slick({
+			infinite: true,
+			arrows: false,
+			slidesToShow: 4,
+			slidesToScroll: 2,
+			speed: 500,
+			dots: true,
+			responsive: [
+				{
+					breakpoint: 1300,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 2,
+					}
+				},
+				{
+					breakpoint: 1100,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2
+					}
+				},
+				{
+					breakpoint: 550,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+					}
+				}
 
-            ]
-        });
-    }
+			]
+		});
+	}
 
-    //contact slider
+	//contact slider
 
 
-    if ($('.address__tabs-items').length > 0 && $(window).width() < 680) {
-        $('.address__tabs-items').slick({
-            infinite: true,
-            arrows: false,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            speed: 500,
-            dots: true,
-        })
-    }
+	//
+	// if ($('.address__tabs-items').length > 0 && $(window).width() < 680) {
+	//     $('.address__tabs-items').slick({
+	//         infinite: true,
+	//         arrows: false,
+	//         slidesToShow: 1,
+	//         slidesToScroll: 1,
+	//         speed: 500,
+	//         dots: true,
+	//     })
+	// }
+
+
+	var interval = setInterval(function () {
+		if (document.readyState === "complete") {
+			setTimeout(function () {
+				if ($('.address__tabs-items').length > 0 && $(window).width() < 680) {
+					$('.address__tabs-items').slick({
+						infinite: true,
+						arrows: false,
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						speed: 500,
+						dots: true,
+					})
+				}
+			}, 100)
+			stopInterval();
+		}
+	}, 1)
+
+	function stopInterval() {
+		clearTimeout(interval);
+	}
 });
