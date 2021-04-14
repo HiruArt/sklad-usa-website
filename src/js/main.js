e6292759cdc930de3dfa3c0acebf39c36c741e8b
ARTEM <girukartem@gmail.com>
2021-04-14 14:56:45 +0300

    about

--- src/js/main.js
+++ src/js/main.js
@@ -32,6 +32,56 @@ if (UAString.indexOf("Trident") !== -1 && UAString.indexOf("rv:10") !== -1) {
 
 $(document).ready(function() {
 
+    if($('[data-max-count]').length > 0) {
+
+        function animateValue(id, start, end, duration) {
+            if (start === end) return;
+            let range = end - start;
+            let current = start;
+            let increment = end > start ? 1 : -1;
+            let stepTime = Math.abs(Math.floor(duration / range));
+            let obj = document.getElementById(id);
+            let timer = setInterval(function () {
+                current += increment;
+                obj.innerHTML = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, stepTime);
+        }
+
+        function showCounterChange() {
+            let counterNum = $('[data-max-count]');
+            counterNum.each(function () {
+                let id = $(this).attr('id');
+                let end = $(this).attr('data-max-count')
+                animateValue(id, 0, end, 2000);
+            });
+        }
+
+        let counterOffsetTop = $('[data-max-count]').offset().top;
+        let screenHeight = $(window).height();
+        let counterFlag = true;
+
+        $(window).scroll(() => {
+            counterOffsetTop = $('[data-max-count]').offset().top - screenHeight;
+
+            if (counterOffsetTop < $(document).scrollTop()
+              && counterFlag){
+                showCounterChange();
+                counterFlag = false;
+            }
+        });
+
+
+        if (counterOffsetTop < $(document).scrollTop() + 300
+          && counterFlag){
+            showCounterChange();
+            counterFlag = false;
+        }
+    }
+
+
     $('.js_mobile-menu').click(function() {
         $(this).closest('header').toggleClass('menu-open');
         $('body').toggleClass('oh');
